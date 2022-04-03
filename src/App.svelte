2606c36a566f6c8bf4dae1d94f7ff14bb49c13aa
jchozas <juanchozass@gmail.com>
2022-04-03 19:32:59 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Cell from './Cell.svelte';
+
+  export let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  ----
+  <Cell />
+</main>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Cell.svelte
@@ -0,0 +1,32 @@
+<script>
+  export const activities = [];
+</script>
+
+<div class="container">
+  <div class="cell"></div>
+</div>
+
+<style>
+  .container {
+    height: 70px;
+    width: 70px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cell {
+    height: 40px;
+    width: 40px;
+    border: 3px solid rgba(42, 24, 42, 0.42);
+    border-radius: 5px;
+    opacity: 0.8;
+    transition: 0.2s;
+  }
+
+  .cell:hover {
+    border: 4px solid rgba(86, 60, 86, 0.6);
+    opacity: 1;
+  }
+</style>
